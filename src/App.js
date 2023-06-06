e4eae47b530b3124dbb08bcfe11fef81f31824c3
Intern-Phurich <intern-phurich@Intern-Phurich.local>
2023-06-06 12:17:07 +0700

    Try to push

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Home.js'
+import Main from './Main.js'
+import { Button } from 'antd';
+
+const App = () => {
+  
+  return (
+    <Router>
+      <div className="App">
+          
+          <Switch>
+
+            <Route exact path="/">
+              <Home />
+              <div className="button">
+                <Link to="/main">
+                  <Button type="primary">Todo List</Button>
+                </Link> 
+              </div>
+              {/* <div className="button"><Button type="dashed" href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target="_blank">Free Nuggets</Button></div> */}
+            </Route>
+
+            <Route path="/main">
+              <Main />
+              <Link to="/">
+                <Button>Home</Button>
+              </Link>
+            </Route>       
+
+          </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
